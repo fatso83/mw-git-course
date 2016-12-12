0ebdb92f6f19404eb1a5d156c07548ecf0c4923b
Arne <bratt@nvg.ntnu.no>
2016-12-12 09:10:55 +0100

    Cleanup

--- src/run-test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem This test file tests your program on Windows
-rem Just update the program variable below to 
-rem reflect how to run your program
-rem
-rem Example: set PROGRAM="node prog.js"
-rem Running: test
