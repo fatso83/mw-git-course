63d74e02f17b7c92b2bb7b445c83293b721d6cd1
Arne <bratt@nvg.ntnu.no>
2016-12-14 14:54:46 +0100

    Adding Fibonacci reference

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-// returns the nth fibonacci sequence f0=0, f1=1, f2=1, f3=2, f4=3, f5=5, f6=8, ...
+// Returns the nth fibonacci sequence f0=0, f1=1, f2=1, f3=2, f4=3, f5=5, f6=8, ...
+// Ref. https://en.wikipedia.org/wiki/Fibonacci_number
 var fibonacci = function f(n) {
     if (n == 0) return 1;
     if (n == 1 || n == 2) return 1;
