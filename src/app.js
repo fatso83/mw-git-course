39e369134f9cba8f3c7c1ffb1564b435a7998258
Arne <bratt@nvg.ntnu.no>
2016-12-14 14:41:56 +0100

    Set up intentional bugs in the code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
-// returns the nth fibonacci sequence f0=0, f1=1, f2=1, f3=3, ...
+// returns the nth fibonacci sequence f0=0, f1=1, f2=1, f3=2, f4=3, f5=5, f6=8, ...
 var fibonacci = function f(n) {
-    if(n == 0) return 0;
-    if(n == 1 || n == 2) return 1;
-    return f(n-2)+f(n-1);
+    if (n == 0) return 1;
+    if (n == 1 || n == 2) return 1;
+    return f(n-2)+f(n-1));
+    }
 }
 
 console.log(fibonacci(process.argv[2]));
